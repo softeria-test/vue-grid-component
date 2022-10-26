e72e707aa886bdd066927310dbd633d565876977
ismailarilik <arilik.ismail@gmail.com>
2022-10-26 16:00:39 +0300

    Clear app

---
+++ src/components/GridComp.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="grid-comp">
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component
+export default class GridComp extends Vue {
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,16 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <GridComp/>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
-import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
+import GridComp from '@/components/GridComp.vue' // @ is an alias to /src
 
 @Component({
   components: {
-    HelloWorld
+    GridComp
   }
 })
 export default class HomeView extends Vue {}
