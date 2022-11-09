d429b1adc0a6a52aa1c947598e4b9cca66848b3a
ismailarilik <arilik.ismail@gmail.com>
2022-11-09 11:42:50 +0000

    Fix SonarScanner issue; a hack to keep a commented code for doc purposes

--- tests/e2e/plugins/index.js
+++ tests/e2e/plugins/index.js
@@ -5,7 +5,6 @@
 // and then use the `file:preprocessor` event
 // as explained in the cypress docs
 // https://docs.cypress.io/api/plugins/preprocessors-api.html#Examples
-
 // /* eslint-disable import/no-extraneous-dependencies, global-require */
 // const webpack = require('@cypress/webpack-preprocessor')
 
