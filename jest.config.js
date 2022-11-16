85fa439591fefcbd04551468232a8b5c349fbc9a
ismailarilik <arilik.ismail@gmail.com>
2022-11-16 09:22:16 +0000

    Enforce a coverage of 80%
    Fixes #7, #8, #11

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,22 @@
 module.exports = {
+  // Indicates whether the coverage information should be collected while executing the test
+  collectCoverage: true,
+
+  // The directory where Jest should output its coverage files
+  coverageDirectory: 'coverage',
+
+  // Indicates which provider should be used to instrument code for coverage
+  coverageProvider: 'v8',
+
+  // An object that configures minimum threshold enforcement for coverage results
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80
+    }
+  },
+
   preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel'
 }
