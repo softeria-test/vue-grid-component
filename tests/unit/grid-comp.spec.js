75ecac03211f2ff26ba805e52f06be4f18d95c13
ismailarilik <arilik.ismail@gmail.com>
2022-12-16 08:58:24 +0000

    test(grid): Fix unit tests
    Since the given prop `table` is async, we should wait it using `await` and `wrapper.setProps` method

--- tests/unit/grid-comp.spec.js
+++ tests/unit/grid-comp.spec.js
@@ -10,65 +10,65 @@ describe('GridComp.vue', () => {
   })
 
   it('renders component properly', async () => {
-    const wrapper = shallowMount(GridComp, {
-      propsData: { table }
-    })
+    const wrapper = await shallowMount(GridComp)
+
+    await wrapper.setProps({ table })
 
     expect(wrapper.text()).toMatch('Example Row Organized Stach Table')
   })
 
-  it('renders correct count of tr elements', () => {
-    const wrapper = shallowMount(GridComp, {
-      propsData: { table }
-    })
+  it('renders correct count of tr elements', async () => {
+    const wrapper = await shallowMount(GridComp)
+
+    await wrapper.setProps({ table })
 
     expect(wrapper.findAll('tr').length).toEqual(18)
   })
 
-  it('renders correct count of th elements (excluding hidden ones)', () => {
-    const wrapper = shallowMount(GridComp, {
-      propsData: { table }
-    })
+  it('renders correct count of th elements (excluding hidden ones)', async () => {
+    const wrapper = await shallowMount(GridComp)
+
+    await wrapper.setProps({ table })
 
     expect(wrapper.findAll('th').length).toEqual(12)
   })
 
-  it('renders correct count of td elements (excluding hidden ones)', () => {
-    const wrapper = shallowMount(GridComp, {
-      propsData: { table }
-    })
+  it('renders correct count of td elements (excluding hidden ones)', async () => {
+    const wrapper = await shallowMount(GridComp)
+
+    await wrapper.setProps({ table })
 
     expect(wrapper.findAll('td').length).toEqual(105)
   })
 
-  it('renders correct rowspan for the first th element', () => {
-    const wrapper = shallowMount(GridComp, {
-      propsData: { table }
-    })
+  it('renders correct rowspan for the first th element', async () => {
+    const wrapper = await shallowMount(GridComp)
+
+    await wrapper.setProps({ table })
 
     expect(wrapper.find('th').attributes('rowspan')).toEqual('3')
   })
 
-  it('renders correct colspan for the second th element', () => {
-    const wrapper = shallowMount(GridComp, {
-      propsData: { table }
-    })
+  it('renders correct colspan for the second th element', async () => {
+    const wrapper = await shallowMount(GridComp)
+
+    await wrapper.setProps({ table })
 
     expect(wrapper.findAll('th').at(1).attributes('colspan')).toEqual('2')
   })
 
-  it('renders correct text-align for the second td element', () => {
-    const wrapper = shallowMount(GridComp, {
-      propsData: { table }
-    })
+  it('renders correct text-align for the second td element', async () => {
+    const wrapper = await shallowMount(GridComp)
+
+    await wrapper.setProps({ table })
 
     expect(wrapper.findAll('td').at(1).attributes('style')).toMatch(/text-align:\s?right/i)
   })
 
-  it('renders correct padding-left for the div of 22nd td element', () => {
-    const wrapper = shallowMount(GridComp, {
-      propsData: { table }
-    })
+  it('renders correct padding-left for the div of 22nd td element', async () => {
+    const wrapper = await shallowMount(GridComp)
+
+    await wrapper.setProps({ table })
 
     expect(wrapper.findAll('td').at(21).find('div').attributes('style')).toMatch(/padding-left:\s?1em/)
   })
